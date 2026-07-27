0b363e6491e17a961229e8e566a31fc0e830bd65
Eamonn de Leastar <edeleastar@wit.ie>
2026-07-27 12:55:05 +0100

    Add @tutors/reader - JSR reader package

---
+++ packages/jsr/tutors/main.ts
@@ -0,0 +1,20 @@
+import {
+  parseCourse,
+  generateDynamicCourse,
+  copyAssets,
+} from "@tutors/tutors-gen-lib";
+import * as fs from "node:fs";
+import process from "node:process";
+
+const versionStr = `tutors: 5.0.5`;
+
+if (!fs.existsSync("course.md")) {
+  console.log("Cannot locate course.md. Please change to course folder and try again.");
+} else {
+  const srcFolder = process.cwd();
+  const destFolder = `${srcFolder}/json`;
+  const [course, lr] = parseCourse(srcFolder);
+  generateDynamicCourse(course, destFolder);
+  copyAssets(lr, destFolder);
+}
+console.log(versionStr);  
