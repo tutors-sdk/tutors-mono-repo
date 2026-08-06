5a0a6b66edeb87a34b0da858bb7497389b030b47
Eamonn de Leastar <edeleastar@wit.ie>
2026-08-06 17:19:44 +0100

    remove hooks from live

--- apps/live/src/hooks.client.ts
+++
@@ -1,8 +0,0 @@
-import type { HandleClientError } from "@sveltejs/kit";
-
-export const handleError: HandleClientError = ({ error }) => {
-  console.error("Live client error:", error);
-  return {
-    message: "An unexpected error occurred"
-  };
-};
